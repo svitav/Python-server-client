39f129e4b14a88fc5a8b726c15cd8c8605b190e6
svitav <57363807+svitav@users.noreply.github.com>
2020-03-10 13:46:52 +0100

    Update klient.py

--- klient.py
+++ klient.py
@@ -4,7 +4,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 2205
 
+
 s.connect((host, port))
-msg = s.recv(1024)
-print(msg.decode('ascii'))
+while True:
+    message = input("->")
+    s.send(bytes(message, "ascii"))
+    msg = s.recv(1024)
+    print(msg.decode('ascii'))
 s.close()
