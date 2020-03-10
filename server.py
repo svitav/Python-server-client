a7cd2fa388046550b84e8fdf1380f7d1609e1783
svitav <57363807+svitav@users.noreply.github.com>
2020-03-10 13:46:24 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import _thread
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -6,10 +7,13 @@ host = socket.gethostname()
 port = 2205
 
 serversocket.bind((host, port))
+serversocket.listen()
+clientsocket, addr = serversocket.accept()
 
 while True:
-    serversocket.listen()
-    clientsocket, addr = serversocket.accept()
-    clientsocket.sendall(b'Hello world!')
-    clientsocket.close()
+    
+    msg = clientsocket.recv(1024)
+    print(msg.decode("ascii"))
+    clientsocket.sendall(bytes(msg))
+clientsocket.close()
     
