be2c3ed96ec753c3ca1ca67274da2f42e9744199
svitav <57363807+svitav@users.noreply.github.com>
2020-03-18 10:51:11 +0100

    Add files via upload

--- klient.py
+++ klient.py
@@ -1,14 +1,23 @@
 import socket
+import threading
+import os
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 port = 2205
 
-
 s.connect((host, port))
+
+def listener():
+    while True:
+        msg = s.recv(1024)
+        print(msg.decode('ascii'))
+
+threading.Thread(None, listener).start()
+
 while True:
     message = input("->")
     s.send(bytes(message, "ascii"))
-    msg = s.recv(1024)
-    print(msg.decode('ascii'))
+    
+    #os.system("clear")
 s.close()
--- server.py
+++ server.py
@@ -1,19 +1,32 @@
 import socket
-import _thread
+import threading
+from time import sleep
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 port = 2205
-
+global clients
+clients = {}
 serversocket.bind((host, port))
-serversocket.listen()
-clientsocket, addr = serversocket.accept()
+def listener():
+    global clients
+    while True:
+        serversocket.listen()
+        clientsocket, addr = serversocket.accept()
+        print(str(addr[0])+" joined!!")
+        clients[len(clients) + 1] = ({"socket": clientsocket, "addr": addr})
+
+threading.Thread(None, listener).start()
+
+
+print("main")
+
+sleep(2)
 
 while True:
-    
-    msg = clientsocket.recv(1024)
-    print(msg.decode("ascii"))
-    clientsocket.sendall(bytes(msg))
-clientsocket.close()
-    
+    for x in range(len(clients)):
+        msg = clients[x].recv(1024)
+        print(msg.decode("ascii"))
+        clients[x].sendall(bytes(client[x]["addr"]+": "+msg))
+#clientsocket.close()
